6e858934977d82825f5e665ee2d23bb675023ba2
Andrei Rat <andreirat@ymail.com>
2020-10-13 14:51:30 +0300

    Updates

---
+++ precache-manifest.ad4cf692d3da3d18a36c1b0af1a29070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a89980e9907758d84a30f46529a648d",
+    "url": "/gh-pages-demo/index.html"
+  },
+  {
+    "revision": "c4045f2e62b37ffe9c1b",
+    "url": "/gh-pages-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a797c86ea3188a89cde9",
+    "url": "/gh-pages-demo/static/js/2.f96a093f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-demo/static/js/2.f96a093f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4045f2e62b37ffe9c1b",
+    "url": "/gh-pages-demo/static/js/main.04d79bb0.chunk.js"
+  },
+  {
+    "revision": "54d62c5b7904e5001992",
+    "url": "/gh-pages-demo/static/js/runtime-main.c8642553.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
